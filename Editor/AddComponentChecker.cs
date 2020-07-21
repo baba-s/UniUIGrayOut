b02bcf24e22e8611e6e26c76c3febd79d8818399
baba_s <shotababa1206@gmail.com>
2020-07-21 21:08:25 +0900

    add checker

---
+++ Editor/AddComponentChecker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	[InitializeOnLoad]
+	internal static class AddComponentChecker
+	{
+		static AddComponentChecker()
+		{
+			ObjectFactory.componentWasAdded += OnComponentWasAdded;
+		}
+
+		private static void OnComponentWasAdded( Component component )
+		{
+			// CanvasRenderer を所持していないゲームオブジェクトには
+			// UIGrayOut コンポーネントをアタッチできないようにする
+
+			// UIGrayOut コンポーネントではない場合は無視
+			if ( !( component is UIGrayOut ) ) return;
+
+			// CanvasRenderer を所持している場合は無視
+			if ( component.GetComponent<CanvasRenderer>() != null ) return;
+			
+			// 1 フレーム遅らせないと下記のエラーが発生する
+			// Assertion failed on expression: 'm_InstanceID != InstanceID_None'
+			EditorApplication.delayCall += () => Object.DestroyImmediate( component );
+		}
+	}
+}
